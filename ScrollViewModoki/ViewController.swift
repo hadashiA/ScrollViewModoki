b9f1c1eca447cede9781e90d11f8ecc6ea963b45
f-kubotar <dev@hadashikick.jp>
2015-12-26 13:40:27 +0900

    Initial Commit

---
+++ ScrollViewModoki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScrollViewModoki
+//
+//  Created by 久保田 竜自 on 2015/12/26.
+//  Copyright © 2015年 hadashikick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
